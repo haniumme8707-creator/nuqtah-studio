5819c62e9884b12c99608ac5b77c86182a0d983e
pixelcraftstudio <haniumme8707@gmail.com>
2026-08-03 23:08:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,8 @@ const navLinks = document.querySelector(".nav-links");
 menuToggle.addEventListener("click", () => {
     navLinks.classList.toggle("active");
 });
+document.querySelectorAll(".nav-links a").forEach(link=>{
+    link.addEventListener("click",()=>{
+        nav.classList.remove("active");
+    });
+});
