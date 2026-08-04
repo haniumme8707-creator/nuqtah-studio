0388abecf0503b4cb8fd05414a0a2160bbe7fe44
pixelcraftstudio <haniumme8707@gmail.com>
2026-08-04 23:56:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 const menuToggle = document.getElementById("menu-toggle");
-const navLinks = document.querySelector(".nav-links");
+const navLinks = document.getElementById("nav-links");
+const closeMenu = document.getElementById("close-menu");
 
+// Open menu
 menuToggle.addEventListener("click", () => {
-    navLinks.classList.toggle("active");
+    navLinks.classList.add("active");
 });
-document.querySelectorAll(".nav-links a").forEach(link=>{
-    link.addEventListener("click",()=>{
-        nav.classList.remove("active");
+
+// Close button
+closeMenu.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+});
+
+// Close after clicking any link
+document.querySelectorAll(".nav-links a").forEach(link => {
+    link.addEventListener("click", () => {
+        navLinks.classList.remove("active");
     });
 });
